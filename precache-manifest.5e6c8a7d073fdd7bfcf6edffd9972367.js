cd90de25d25d8dc0906aefa73f4e055e99a24f09
shrinathjoshi <shrinathjoshi1997@gmail.com>
2019-12-24 14:36:45 +0530

    Updates

---
+++ precache-manifest.5e6c8a7d073fdd7bfcf6edffd9972367.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6782798ac7323ce468758b97ebfed6e5",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "33bc6447f68ec81b0d6a",
+    "url": "/monster-rolodex/static/css/main.39787c32.chunk.css"
+  },
+  {
+    "revision": "dcd2657b3ef8f78d14f4",
+    "url": "/monster-rolodex/static/js/2.e6fbc2c9.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monster-rolodex/static/js/2.e6fbc2c9.chunk.js.LICENSE"
+  },
+  {
+    "revision": "33bc6447f68ec81b0d6a",
+    "url": "/monster-rolodex/static/js/main.0d12564a.chunk.js"
+  },
+  {
+    "revision": "34cc0ee46c09165226f7",
+    "url": "/monster-rolodex/static/js/runtime-main.0c1ff8b6.js"
+  }
+]);
